7c6741296ff9f0d332d39e977dc2dd89aff7dff7
thisisaaronland <aaron@mapzen.com>
2015-07-27 19:55:46 +0000

    import airports (from woedb)

--- mapzen/whosonfirst/importer/__init__.py
+++ mapzen/whosonfirst/importer/__init__.py
@@ -3,6 +3,10 @@ __import__('pkg_resources').declare_namespace(__name__)
 
 import mapzen.whosonfirst.export
 
+import shapely.geometry
+import requests
+import json
+
 class base(mapzen.whosonfirst.export.flatfile):
 
     def __init__(self, root, **kwargs):
@@ -11,5 +15,66 @@ class base(mapzen.whosonfirst.export.flatfile):
 
     def import_feature(self, feature, **kwargs):
 
+        if self.concordances_db:
+
+            # Note this is *before* we have massage_feature-ed
+            # the feature so the ID will be bound to the properties
+            # and not a 'wof:concordances' property
+
+            lookup = props.get(self.concordances_key, None)
+
+            if lookup:
+                wofid = self.concordances_db.woe_id(lookup)
+                logging.debug("got %s for %s" % (wofid, lookup))
+
+                if wofid != 0:
+                    return True
+
         # as in mapzen.whosonfirst.export.flatfile.export_feature
+
         return self.export_feature(feature, **kwargs)
+
+    # maybe put this in mapzen.whosonfirst.export as 'ensure_hierarchy' ?
+    # (20150727/thisisaaronland)
+
+    def append_hierarchy(self, feature, **kwargs):
+
+        props = feature['properties']
+
+        hier = []
+
+        geom = f['geometry']
+        shp = shapely.geometry.asShape(geom)
+        coords = shp.centroid
+
+        lat = coords.y
+        lon = coords.x
+
+        for pt in ('neighbourhood', 'locality', 'region', 'country'):
+
+            try:
+                params = {'latitude': lat, 'longitude': lon, 'placetype': pt}
+                rsp = requests.get('https://54.148.56.3/', params=params, verify=False)
+                
+                data = json.loads(rsp.content)
+            except Exception, e:
+                logging.error(e)
+                continue
+
+            if len(data['features']) == 1:
+                props['wof:parent_id'] = data['features'][0]['id']
+
+            if len(data['features']) >= 1:
+
+                for pf in data['features']:
+                    pp = pf['properties']
+
+                    if pp.get('wof:hierarchy', False):
+                        hier.extend(pp['wof:hierarchy'])
+
+                break
+
+        props['wof:hierarchy'] = hier
+
+        feature['properties'] = props
+        
--- mapzen/whosonfirst/importer/woedb.py
+++ mapzen/whosonfirst/importer/woedb.py
@@ -17,3 +17,67 @@ class timezone_importer(mapzen.whosonfirst.importer.base):
             props['woe:source'] = woe_props['provider']
 
         f['properties'] = props
+
+
+class airport_importer(mapzen.whosonfirst.importer.base):
+
+    def massage_feature(self, f):
+
+        woe_props = f['properties']
+
+        props = {}
+        props['wof:placetype'] = 'campus'
+        props['wof:source'] = 'woedb'
+        props['wof:name'] = woe_props.get('name', '')
+
+        props['wof:parent_id'] = -1
+
+        if woe_props.get('fullname', None) :
+            props['wof:fullname'] = woe_props['wof:fullname']
+
+        concordances = {}
+
+        for k in ('woe:id', 'iata:code', 'icao:code'):
+
+            if woe_props.get(k, False):
+                concordances[k] = woe_props[k]
+
+        props['wof:concordances'] = concordances
+
+        if woe_props.get('provider', False):
+            props['woe:source'] = woe_props['provider']
+
+        if woe_props.get('iso', False):
+            props['iso_country'] = woe_props['iso']
+            
+        for k, v in woe_props.items():
+
+            if not k.startswith("alias"):
+                continue
+
+            k = k.lower()
+
+            ignore, lang, flag = k.split("_")
+
+            if flag == "q":
+                flag = "p"
+
+            k = "name:%s_%s" % (lang, flag)
+
+            props[k] = v
+
+        #
+
+        woe_hier = {}
+
+        for k, v in woe_props.get('hierarchy', {}):
+            k = "%s_id" % k
+            woe_hier[k] = v
+
+        if len(woe_hier.keys()):
+            props['woe:hierarchy'] = woe_hier
+
+
+        f['properties'] = props
+
+        self.append_hierarchy(f)
