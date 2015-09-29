ccbbc900274e5237fa442ffe802680549a5f5823
thisisaaronland <aaron@mapzen.com>
2015-09-29 23:14:02 +0000

    update dependency links; bump to 0.04

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ desc = open("README.md").read(),
 setup(
     name='mapzen.whosonfirst.importer',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.importer'],
-    version='0.03',
+    version='0.04',
     description='Simple Python wrapper for managing Who\'s On First import-related functions',
     author='Mapzen',
     url='https://github.com/thisisaaronland/py-mapzen-gazetter-import',
@@ -20,10 +20,12 @@ setup(
         'mapzen.whosonfirst.spatial',
         ],
     dependency_links=[
-        'https://github.com/mapzen/py-mapzen-whosonfirst-export/tarball/master#egg=mapzen-whosonfirst-export-0.39',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-export/tarball/master#egg=mapzen.whosonfirst.export-0.59',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-concordances/tarball/master#egg=mapzen.whosonfirst.concordances-0.01',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-spatial/tarball/master#egg=mapzen.whosonfirst.spatial-0.03',
         ],
     packages=packages,
     scripts=[
         ],
-    download_url='https://github.com/thisisaaronland/py-mapzen-whosonfirst-import/releases/tag/v0.03',
+    download_url='https://github.com/thisisaaronland/py-mapzen-whosonfirst-import/releases/tag/v0.04',
     license='BSD')
