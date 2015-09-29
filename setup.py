4a2ef364e71a8c91d56ef4e42028dabdbc9c2bf2
thisisaaronland <aaron@mapzen.com>
2015-09-29 23:43:27 +0000

    python setup wrangling; bump to 0.07

--- setup.py
+++ setup.py
@@ -8,25 +8,25 @@ desc = open("README.md").read(),
 setup(
     name='mapzen.whosonfirst.importer',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.importer'],
-    version='0.06',
+    version='0.07',
     description='Simple Python wrapper for managing Who\'s On First import-related functions',
     author='Mapzen',
     url='https://github.com/thisisaaronland/py-mapzen-gazetter-import',
     install_requires=[
         'requests',
         'geojson',
-        'mapzen.whosonfirst.export',
-        'mapzen.whosonfirst.concordances',
-        'mapzen.whosonfirst.spatial',
+        'mapzen.whosonfirst.export>=0.59',
+        'mapzen.whosonfirst.concordances>=0.01',
+        'mapzen.whosonfirst.spatial>=0.04',
         ],
     dependency_links=[
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-export/tarball/master#egg=mapzen.whosonfirst.export-0.59',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-concordances/tarball/master#egg=mapzen.whosonfirst.concordances-0.01',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-spatial/tarball/master#egg=mapzen.whosonfirst.spatial-0.03',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-export/tarball/master#egg=mapzen.whosonfirst.export',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-concordances/tarball/master#egg=mapzen.whosonfirst.concordances',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-spatial/tarball/master#egg=mapzen.whosonfirst.spatial',
         ],
     packages=packages,
     scripts=[
         'scripts/wof-importify'
         ],
-    download_url='https://github.com/thisisaaronland/py-mapzen-whosonfirst-import/releases/tag/v0.06',
+    download_url='https://github.com/thisisaaronland/py-mapzen-whosonfirst-import/releases/tag/v0.07',
     license='BSD')
